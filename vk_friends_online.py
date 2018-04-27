1ae4e7a8d89626ab164af83d9526c874fdff403e
Z4PH0D <dreamfall3r@gmail.com>
2018-04-27 20:46:06 +0200

    Поправлено по замечаниям

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,7 +1,5 @@
 import vk
 import getpass
-from time import sleep
-from sys import exit
 
 
 APP_ID = 6462270
@@ -15,33 +13,32 @@ def get_user_password():
     return getpass.getpass()
 
 
-def get_session(login, password):
+def get_session(login, password, v_api='5.3.5'):
     session = vk.AuthSession(
             app_id=APP_ID,
             user_login=login,
             user_password=password,
             scope='users, friends',
             )
-    return vk.API(session, v='5.3.5')
+    return vk.API(session, v=v_api)
 
 
-def get_online_friends(session):
+def get_online_friend_ids(session):
     return session.friends.getOnline()
 
 
-def get_name(session, user_id):
-    return session.users.get(user_id=user_id, fields='first_name, last_name')
+def get_friends_names(session, online_friend_ids):
+    return session.users.get(
+        user_ids=online_friend_ids,
+        fields='first_name, last_name')
 
 
 def output_friends_to_console(friends_online):
-    sleep_time = 0.3
     print('Friends online:')
     for friend in friends_online:
-        user_info = get_name(session, friend)
-        first_name = user_info[0]['first_name']
-        last_name = user_info[0]['last_name']
+        first_name = friend['first_name']
+        last_name = friend['last_name']
         print(first_name, last_name)
-        sleep(sleep_time)
 
 
 if __name__ == '__main__':
@@ -51,5 +48,6 @@ if __name__ == '__main__':
         session = get_session(login, password)
     except vk.exceptions.VkAuthError:
         exit('Incorrect login\password')
-    friends_online = get_online_friends(session)
-    output_friends_to_console(friends_online)
+    friends_online = get_online_friend_ids(session)
+    online_friends_names = get_friends_names(session, friends_online)
+    output_friends_to_console(online_friends_names)
