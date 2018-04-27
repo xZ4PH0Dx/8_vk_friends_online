7db6ba1181a65357ad0d7f7aaefb3f1f15dfcebc
Z4PH0D <dreamfall3r@gmail.com>
2018-04-27 15:59:00 +0200

    Первая версия

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,54 @@
 import vk
+import getpass
+from time import sleep
+from sys import exit
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6462270
 
 
 def get_user_login():
-    pass
+    return input('Pass your login\\number here:\n')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass()
 
 
-def get_online_friends(login, password):
+def get_session(login, password):
     session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+            app_id=APP_ID,
+            user_login=login,
+            user_password=password,
+            scope='users, friends',
+            )
+    return vk.API(session, v='5.3.5')
+
+
+def get_online_friends(session):
+    return session.friends.getOnline()
+
+
+def get_name(session, user_id):
+    return session.users.get(user_id=user_id, fields='first_name, last_name')
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Friends online:')
+    for friend in friends_online:
+        user_info = get_name(session, friend)
+        first_name = user_info[0]['first_name']
+        last_name = user_info[0]['last_name']
+        print(first_name, last_name)
+        sleep(0.3)
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
+    try:
+        session = get_session(login, password)
+    except vk.exceptions.VkAuthError:
+        exit('Incorrect login\password')
+    friends_online = get_online_friends(session)
     output_friends_to_console(friends_online)
